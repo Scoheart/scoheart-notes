df95156f5e0f51b72127098e0246f48bd3829635
Scoheart <Scoheart1487790559@gmail.com>
2025-08-11 10:44:25 +0800

    feat: introduce new string formatting options and enhance validation checks for improved usability

---
+++ docs/408/dsa/leetcode_cn/70. 爬楼梯/a.ts
@@ -0,0 +1,17 @@
+import test from 'node:test';
+import assert from 'node:assert';
+
+
+test('testcase', () => {
+
+});
+
+function climbStairs(n: number): number {
+    const dp = new Array(n).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
